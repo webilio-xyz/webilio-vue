480a2dbfd665d05bcf9b8c5f5bc985dd8ee3203b
Vincent Blais <vincent.blais@webilio.ca>
2024-10-15 10:38:14 -0400

    Exported columns models

---
+++ src/Models/index.js
@@ -0,0 +1,11 @@
+import Columns from "./Columns";
+import ColumnDefinition from "./ColumnDefinition";
+export {
+    Columns,
+    ColumnDefinition
+}
+
+export default {
+    Columns,
+    ColumnDefinition
+}
