2c8e088ba008e81d446fcd0d9287ce7580350642
Vincent Blais <vincent.blais@webilio.ca>
2025-03-19 13:28:37 -0400

    Fix export

--- src/Components/Component/index.js
+++ src/Components/Component/index.js
@@ -1,12 +1,39 @@
 import AlertComponent from "./AlertComponent.vue";
 import ButtonComponent from "./ButtonComponent.vue";
+import AvatarComponent from "./AvatarComponent.vue";
+import BreadcrumbsComponent from "./BreadcrumbsComponent.vue";
+import DropzoneComponent from "./DropzoneComponent.vue";
+import FoldableComponent from "./FoldableComponent.vue";
+import InfiniteScrollComponent from "./InfiniteScrollComponent.vue";
+import NotesCommentComponent from "./NotesCommentComponent.vue";
+import NotesInputComponent from "./NotesInputComponent.vue";
+import StepsComponent from "./StepsComponent.vue";
+import TagComponent from "./TagComponent.vue";
 
 export {
     AlertComponent,
-    ButtonComponent
+    ButtonComponent,
+    AvatarComponent,
+    BreadcrumbsComponent,
+    DropzoneComponent,
+    FoldableComponent,
+    InfiniteScrollComponent,
+    NotesCommentComponent,
+    NotesInputComponent,
+    StepsComponent,
+    TagComponent
 };
 
 export default {
     AlertComponent,
-    ButtonComponent
+    ButtonComponent,
+    AvatarComponent,
+    BreadcrumbsComponent,
+    DropzoneComponent,
+    FoldableComponent,
+    InfiniteScrollComponent,
+    NotesCommentComponent,
+    NotesInputComponent,
+    StepsComponent,
+    TagComponent
 };
