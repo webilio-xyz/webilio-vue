54b136cea91e087b5aca39320b54a73de4421aea
Vincent Blais <vincent.blais@webilio.ca>
2026-04-03 11:35:58 -0400

    Added vite-plugin-css-injected-by-js to inject scoped styles into JS bundles

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        cssInjectedByJsPlugin({
+            relativeCSSInjection: true,
+        }),
+    ],
     build: {
         lib: {
             entry: {
